e3b08aeb98b4f8ae04a7a21cf9e966717e4d1136
hamasyou <hamasyou@gmail.com>
2014-02-13 01:33:00 +0900

    fix

--- source/javascripts/hamasyou.js
+++ source/javascripts/hamasyou.js
@@ -6,5 +6,14 @@ jQuery(function($) {
         $('body,html').animate({
             scrollTop: 0
         }, 400);
+        return false;
+    });
+
+    $('a[href="#disqus_thread"]').on('click', function() {
+        var p =$('#disqus_thread').offset().top;
+        $('body,html').animate({
+            scrollTop: p
+        }, 400);
+        return false;
     });
 });
