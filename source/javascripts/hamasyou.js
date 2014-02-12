b0b2ce7b2337face83d3a1a4242fdd4c0c479100
hamasyou <hamasyou@gmail.com>
2014-02-12 17:00:04 +0900

    fix

--- source/javascripts/hamasyou.js
+++ source/javascripts/hamasyou.js
@@ -1,4 +1,10 @@
 jQuery(function($) {
     FastClick.attach(document.body);
     $('a[rel~="external"]').attr('target', '_blank');
+
+    $('#scroll-to-top').on('click', function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+    });
 });
