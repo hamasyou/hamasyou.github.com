cf6b6c2907538cf6ad7af468c9e09e0c19d63e97
hamasyou <hamasyou@gmail.com>
2014-02-14 23:55:32 +0900

    add new post

--- source/javascripts/hamasyou.js
+++ source/javascripts/hamasyou.js
@@ -2,7 +2,7 @@ jQuery(function($) {
     FastClick.attach(document.body);
     $('a[rel~="external"]').attr('target', '_blank');
 
-    $('#scroll-to-top').on('click', function() {
+    $('.scroll-to-top').on('click', function() {
         $('body,html').animate({
             scrollTop: 0
         }, 400);
