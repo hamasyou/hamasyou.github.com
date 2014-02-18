9a8b8d9b7b1b9551b292b005f9c51a3eb9775a4c
hamasyou <hamasyou@gmail.com>
2014-02-18 15:09:04 +0900

    fix

--- source/javascripts/hamasyou.js
+++ source/javascripts/hamasyou.js
@@ -16,16 +16,4 @@ jQuery(function($) {
         }, 400);
         return false;
     });
-
-    <!-- slide in/out -->
-    $('a.excerpt-link').click(function() {
-        var pass = $(this).attr('href');
-        $('#container').transition({x: -$(window).width() + 'px', opacity: '0'}, 500, function() {
-            location.href = pass;
-            setTimeout(function() {
-                $('#container').css({marginLeft: '0', opacity: '1'})
-            }, 100);
-        });
-        return false;
-    });
 });
