8ca0bf541ba54ca2b777da92524dd7526d7a1554
hamasyou <hamasyou@gmail.com>
2014-02-18 12:07:16 +0900

    fix

--- source/javascripts/hamasyou.js
+++ source/javascripts/hamasyou.js
@@ -10,10 +10,22 @@ jQuery(function($) {
     });
 
     $('a[href="#disqus_thread"]').on('click', function() {
-        var p =$('#disqus_thread').offset().top;
+        var p = $('#disqus_thread').offset().top;
         $('body,html').animate({
             scrollTop: p
         }, 400);
+        return false;
+    });
+
+    <!-- slide in/out -->
+    $('a.excerpt-link').click(function() {
+        var pass = $(this).attr('href');
+        $('#container').transition({x: -$(window).width() + 'px', opacity: '0'}, 500, function() {
+            location.href = pass;
+            setTimeout(function() {
+                $('#container').css({marginLeft: '0', opacity: '1'})
+            }, 100);
+        });
         return false;
     });
 });
